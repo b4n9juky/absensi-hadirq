bf3dbc7340a435133c3e1ec0748b2fa138b690dc
b4n9juky <marzuki.bustan.sulaiman@gmail.com>
2026-07-15 18:39:54 +0700

    chore: remove test files

--- backend/list_users.js
+++
@@ -1,9 +0,0 @@
-require('./dist/lib/env.js');
-const mysql = require('mysql2/promise');
-async function main() {
-  const c = await mysql.createConnection(process.env.DATABASE_URL);
-  const [r] = await c.query("SELECT email, role FROM user");
-  console.log(JSON.stringify(r));
-  await c.end();
-}
-main().catch(e => console.error(e.message));
